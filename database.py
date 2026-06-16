0d6ecc9c2926b0092d2e5cbedfdcf52f04eca78b
Mohammed Akmal <mohammedakmal1493@gmail.com>
2026-06-16 20:22:25 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,16 +8,6 @@ engine = create_engine(settings_instance.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-engine = create_engine(
-    settings.DATABASE_URL, 
-    pool_size=10, 
-    max_overflow=20,
-    pool_pre_ping=True
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
 def get_db():
     db = SessionLocal()
     try:
